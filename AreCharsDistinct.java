cd77b91e1e762129b43796675a1906d534d9a1dd
Dogac Akyıldız <ndakyildiz@gmail.com>
2019-12-07 17:51:56 +0300

    Update AreCharsDistinct.java

--- AreCharsDistinct.java
+++ AreCharsDistinct.java
@@ -18,4 +18,28 @@ public class App {
 
         return true;
     }
+    
+    // Following method is O(N) time complexity
+    // O(1) space complexity
+    private static final int MAX_CHARS = 256;
+
+    public static boolean areCharsDistinctAscii(String str) {
+        int length = str.length();
+
+        if (length > MAX_CHARS)
+            return true;
+
+        boolean [] flags = new boolean[MAX_CHARS];
+
+        for (int i = 0; i < length; i++) {
+            int charCode = str.charAt(i);
+
+            if (flags[charCode])
+                return false;
+
+            flags[charCode] = true;
+        }
+
+        return true;
+    }
 }
