fd6a7f6bd10b93dff4a7d9bd5e42e0dbc27117c6
Dogac Akyıldız <ndakyildiz@gmail.com>
2020-03-15 20:13:39 +0300

    Create VarArgsErrors.java

---
+++ VarArgsErrors.java
@@ -0,0 +1,17 @@
+public class App
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    
+    private static void display(double... a, int... b) // Error 
+    {
+        
+    }
+
+    private static void display(short... a, int b) // Error 
+    {
+
+    }
+}
